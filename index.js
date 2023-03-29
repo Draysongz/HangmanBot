22c8b36923bf696040e914e1ac8a27abbf49acf3
Ackermann <dharmiegra4@gmail.com>
2023-03-29 16:24:32 +0100

    fixed others

--- index.js
+++ index.js
@@ -7,83 +7,85 @@ const apiUrl = 'https://api.api-ninjas.com/v1/randomword';
 let gameInProgress = false;
 const games = {};
 function hangman(msg) {
-    // Retrieve a random word from the API
-    axios
-      .get(apiUrl, {
-        headers: {
-          'X-Api-Key': 'qNA7BTKA279oPvKYq8Q/nw==0IFEzyslpydbeA8S',
-        },
-      })
-      .then((response) => {
-        const word = response.data.word.toLowerCase();
-        console.log(word)
-        let guessedLetters = new Set();
-        let lives = 6;
-        let result = '_'.repeat(word.length);
+  try {
+    const word = randomWords({maxLength: 4})
+    console.log(word)
+  } catch (error) {
+    throw error
+    gameInProgress= false
+  }
+}
+  //   // Retrieve a random word from the API
+  // try{
+  //       const word = response.data.word.toLowerCase();
+  //       console.log(word)
+  //       let guessedLetters = new Set();
+  //       let lives = 6;
+  //       let result = '_'.repeat(word.length);
   
-        //  welcome message and chatId
-        const chatId = msg.chat.id;
-        bot.sendMessage(chatId, `Welcome to Hangman! Your word has ${word.length} letters.`);
+  //       //  welcome message and chatId
+  //       const chatId = msg.chat.id;
+  //       bot.sendMessage(chatId, `Welcome to Hangman! Your word has ${word.length} letters.`);
   
-        // Handle user input
-        bot.on('message', (msg) => {
-          if (msg.chat.id !== chatId) {
-            return;
-          }
+  //       // Handle user input
+  //       bot.on('message', (msg) => {
+  //         if (msg.chat.id !== chatId) {
+  //           return;
+  //         }
   
-          const letter = msg.text.toLowerCase();
+  //         const letter = msg.text.toLowerCase();
         
           
-          if (guessedLetters.has(letter)) {
-            bot.sendMessage(chatId, `You already guessed the letter "${letter}".`);
-            return;
-          }
+  //         if (guessedLetters.has(letter)) {
+  //           bot.sendMessage(chatId, `You already guessed the letter "${letter}".`);
+  //           return;
+  //         }
   
-          // Add the letter to the set of guessed letters
-          guessedLetters.add(letter);
+  //         // Add the letter to the set of guessed letters
+  //         guessedLetters.add(letter);
   
-          // Check if the letter is in the word
-          if (word.includes(letter)) {
-            // Update the result string
-            for (let i = 0; i < word.length; i++) {
-              if (word[i] === letter) {
-                result = result.substring(0, i) + letter + result.substring(i + 1);
-              }
-            }
+  //         // Check if the letter is in the word
+  //         if (word.includes(letter)) {
+  //           // Update the result string
+  //           for (let i = 0; i < word.length; i++) {
+  //             if (word[i] === letter) {
+  //               result = result.substring(0, i) + letter + result.substring(i + 1);
+  //             }
+  //           }
   
-            // Check if the word has been completely guessed
-            if (!result.includes('_')) {
-              bot.sendMessage(chatId, `Congratulations! You guessed the word "${word}"`);
-              bot.removeAllListeners('message');
-              gameInProgress = false;
-              return;
-            }
+  //           // Check if the word has been completely guessed
+  //           if (!result.includes('_')) {
+  //             bot.sendMessage(chatId, `Congratulations! You guessed the word "${word}"`);
+  //             bot.removeAllListeners('message');
+  //             gameInProgress = false;
+  //             return;
+  //           }
   
-            bot.sendMessage(chatId, `Good guess! The word now looks like this: ${result}`);
-          } else {
-            // Decrement lives and check if the game is over
-            if(letter.length > 1){
-                bot.sendMessage(chatId, 'Please enter a letter per trial')
-              }else{
-            lives--;
-            if (lives === 0) {
-              bot.sendMessage(chatId, `Sorry, you ran out of lives. The word was "${word}".`);
-              bot.removeAllListeners('message');
-              gameInProgress = false;
-              return;
-            }
+  //           bot.sendMessage(chatId, `Good guess! The word now looks like this: ${result}`);
+  //         } else {
+  //           // Decrement lives and check if the game is over
+  //           if(letter.length > 1){
+  //               bot.sendMessage(chatId, 'Please enter a letter per trial')
+  //             }else{
+  //           lives--;
+  //           if (lives === 0) {
+  //             bot.sendMessage(chatId, `Sorry, you ran out of lives. The word was "${word}".`);
+  //             bot.removeAllListeners('message');
+  //             gameInProgress = false;
+  //             return;
+  //           }
   
-            bot.sendMessage(chatId, `Bad luck! The letter "${letter}" is not in the word. You have ${lives} lives left.`);
-          }
-        }
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        bot.sendMessage(msg.chat.id, 'Sorry, something went wrong. Please try again later.');
-        gameInProgress= false
-      });
-  }
+  //           bot.sendMessage(chatId, `Bad luck! The letter "${letter}" is not in the word. You have ${lives} lives left.`);
+  //         }
+  //       }
+  //       });
+  //     })
+  //     catch((error) => {
+  //       console.error(error);
+  //       bot.sendMessage(msg.chat.id, 'Sorry, something went wrong. Please try again later.');
+  //       gameInProgress= false
+  //     });
+  // }
  
 
   bot.onText(/\/single/, (msg) => {
