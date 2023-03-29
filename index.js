5d2634513354ac8ed21a1a7c4c0c2a3dbb9df501
Ackermann <dharmiegra4@gmail.com>
2023-03-29 17:00:58 +0100

    movinf

--- index.js
+++ index.js
@@ -1,105 +1,84 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
+const randomWords = require('random-words');
+
 const token = '6106706434:AAFW_RoIgDywRQY4WyZYSAx1mN4ABjdFCcw';
 const bot = new TelegramBot(token, { polling: true });
-const apiUrl = 'https://api.api-ninjas.com/v1/randomword';
 
 let gameInProgress = false;
 const games = {};
+
 function hangman(msg) {
   try {
-    const word = randomWords({maxLength: 4})
-    console.log(word)
+    const word = randomWords();
+    console.log(word);
+    let guessedLetters = new Set();
+    let lives = 6;
+    let result = '_'.repeat(word.length);
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `Let's begin! Your word has ${word.length} letters.`);
+
+    while (result.includes('_') && lives > 0) {
+      bot.on('message', (msg) => {
+        const letter = msg.text.toLowerCase();
+        if (guessedLetters.has(letter)) {
+          bot.sendMessage(chatId, `You already guessed the letter "${letter}"`);
+        } else {
+          guessedLetters.add(letter);
+          if (word.includes(letter)) {
+            result = result.split('').map((char, index) => {
+              if (word[index] === letter) {
+                return letter;
+              } else {
+                return char;
+              }
+            }).join('');
+            bot.sendMessage(chatId, `Good guess! The word now looks like this: ${result}`);
+          } else {
+            lives--;
+            if (lives === 0) {
+              bot.sendMessage(chatId, `Sorry, you ran out of lives. The word was "${word}".`);
+              bot.removeAllListeners('message');
+              gameInProgress = false;
+              return;
+            }
+            bot.sendMessage(chatId, `Bad luck! The letter "${letter}" is not in the word. You have ${lives} lives left.`);
+          }
+          // Check if the word has been completely guessed
+          if (!result.includes('_')) {
+            bot.sendMessage(chatId, `Congratulations! You guessed the word "${word}"`);
+            bot.removeAllListeners('message');
+            gameInProgress = false;
+            return;
+          }
+        }
+      });
+    }
   } catch (error) {
-    throw error
-    gameInProgress= false
+    throw error;
+    gameInProgress = false;
   }
 }
-  //   // Retrieve a random word from the API
-  // try{
-  //       const word = response.data.word.toLowerCase();
-  //       console.log(word)
-  //       let guessedLetters = new Set();
-  //       let lives = 6;
-  //       let result = '_'.repeat(word.length);
-  
-  //       //  welcome message and chatId
-  //       const chatId = msg.chat.id;
-  //       bot.sendMessage(chatId, `Welcome to Hangman! Your word has ${word.length} letters.`);
-  
-  //       // Handle user input
-  //       bot.on('message', (msg) => {
-  //         if (msg.chat.id !== chatId) {
-  //           return;
-  //         }
-  
-  //         const letter = msg.text.toLowerCase();
-        
-          
-  //         if (guessedLetters.has(letter)) {
-  //           bot.sendMessage(chatId, `You already guessed the letter "${letter}".`);
-  //           return;
-  //         }
-  
-  //         // Add the letter to the set of guessed letters
-  //         guessedLetters.add(letter);
-  
-  //         // Check if the letter is in the word
-  //         if (word.includes(letter)) {
-  //           // Update the result string
-  //           for (let i = 0; i < word.length; i++) {
-  //             if (word[i] === letter) {
-  //               result = result.substring(0, i) + letter + result.substring(i + 1);
-  //             }
-  //           }
-  
-  //           // Check if the word has been completely guessed
-  //           if (!result.includes('_')) {
-  //             bot.sendMessage(chatId, `Congratulations! You guessed the word "${word}"`);
-  //             bot.removeAllListeners('message');
-  //             gameInProgress = false;
-  //             return;
-  //           }
-  
-  //           bot.sendMessage(chatId, `Good guess! The word now looks like this: ${result}`);
-  //         } else {
-  //           // Decrement lives and check if the game is over
-  //           if(letter.length > 1){
-  //               bot.sendMessage(chatId, 'Please enter a letter per trial')
-  //             }else{
-  //           lives--;
-  //           if (lives === 0) {
-  //             bot.sendMessage(chatId, `Sorry, you ran out of lives. The word was "${word}".`);
-  //             bot.removeAllListeners('message');
-  //             gameInProgress = false;
-  //             return;
-  //           }
-  
-  //           bot.sendMessage(chatId, `Bad luck! The letter "${letter}" is not in the word. You have ${lives} lives left.`);
-  //         }
-  //       }
-  //       });
-  //     })
-  //     catch((error) => {
-  //       console.error(error);
-  //       bot.sendMessage(msg.chat.id, 'Sorry, something went wrong. Please try again later.');
-  //       gameInProgress= false
-  //     });
-  // }
- 
 
-  bot.onText(/\/single/, (msg) => {
-    if (gameInProgress) {
-      bot.sendMessage(msg.chat.id, 'Sorry, a game is already in progress.');
-    } else {
-      gameInProgress = true;
-      // start the game
-      hangman(msg);
-    }
-   
-  });  
-  bot.onText(/\/start/, (msg) => {
-    const username = msg.from.username;
-    bot.sendMessage(msg.chat.id, `Hello ${username}, Welcome to the hangman bot.`);
-  });  
+
+bot.onText(/\/single/, (msg) => {
+  const chatId= msg.chat.id
+  if (gameInProgress) {
+    bot.sendMessage(msg.chat.id, 'Sorry, a game is already in progress.');
+  } else {
+    gameInProgress = true;
+    // Start the game
+    hangman(chatId);
+  }
+});
+
+bot.onText(/\/start/, (msg) => {
+  const username = msg.from.username;
+  bot.sendMessage(msg.chat.id, `Hello ${username}, welcome to the Hangman Bot.`);
+});
+
+bot.onText(/\/stop/, (msg) => {
+ gameInProgress= false
+ bot.sendMessage(msg.chat.id, `Game stopped`);
+});
 
