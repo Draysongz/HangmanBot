96507d78bf7ad50bb7bfe0e4f41624ab88a0fe37
Ackermann <dharmiegra4@gmail.com>
2023-04-17 05:11:06 +0100

    done multi

--- index.js
+++ index.js
@@ -89,69 +89,156 @@ let gameInProgress = false;
 let gameStarting = false;
 let gameTimer;
 let players = [];
+let guessedLetters = new Set();
 
-function startRound(currentPlayer, word, guessedLetters, players) {
-  let result = '_'.repeat(word.length);
+
+function debounce(callback, delay) {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
+
+async function startRound(currentPlayer, word, guessedLetters, wordState) {
   let gameOver = false;
 
-  bot.sendMessage(currentPlayer.chatId, `@${currentPlayer.username}, you have ${currentPlayer.lives} lives left. Guess a letter!`);
+  await bot.sendMessage(currentPlayer.chatId, `The Word has ${word.length} characters!`);
 
-  const messageHandler = (msg) => {
+  await bot.sendMessage(currentPlayer.chatId, `@${currentPlayer.username}, you have ${currentPlayer.lives} lives left. Guess a letter!`);
+
+  const messageHandler = debounce(async (msg) => {
     if (msg.from.username !== currentPlayer.username) {
       return;
     }
 
     const letter = msg.text.toLowerCase();
+    if (letter.length > 1) {
+      await bot.sendMessage(currentPlayer.chatId,`@${currentPlayer.username}, please enter only one letter at a time.`);
+      return;
+    }
 
     if (guessedLetters.has(letter)) {
-      bot.sendMessage(currentPlayer.chatId, `@${currentPlayer.username}, you already guessed the letter "${letter}"`);
+      await bot.sendMessage(currentPlayer.chatId,` @${currentPlayer.username}, The letter "${letter}" has been guessed`);
     } else {
       guessedLetters.add(letter);
 
-      if (word.includes(letter)) {
-        result = result.split('').map((char, index) => {
-          if (word[index] === letter) {
-            return letter;
-          } else {
-            return char;
-          }
-        }).join('');
-        bot.sendMessage(currentPlayer.chatId, `@${currentPlayer.username}, good guess! The word now looks like this: ${result}`);
-      } else {
+      if (!word.includes(letter)) {
         currentPlayer.lives--;
         if (currentPlayer.lives === 0) {
-          bot.sendMessage(currentPlayer.chatId, `@${currentPlayer.username}, sorry, you ran out of lives. The word was "${word}".`);
+          await bot.sendMessage(currentPlayer.chatId, `@${currentPlayer.username}, sorry, you ran out of lives`);
           players.splice(players.indexOf(currentPlayer), 1);
         } else {
-          bot.sendMessage(currentPlayer.chatId, `@${currentPlayer.username}, bad luck! The letter "${letter}" is not in the word. You have ${currentPlayer.lives} lives left.`);
+          await bot.sendMessage(currentPlayer.chatId,` @${currentPlayer.username}, bad luck! The letter "${letter}" is not in the word. You have ${currentPlayer.lives} lives left.`);
+          await bot.sendMessage(currentPlayer.chatId, ` The word now looks like this: ${wordState.join(' ')}`);
+        }
+      } else {
+        for (let i = 0; i < word.length; i++) {
+          if (word[i] === letter) {
+            wordState[i] = letter;
+          }
+        }
+
+        if (!wordState.includes(null)) {
+          const playersWithLives = players.filter(player => player.lives > 0);
+          if (playersWithLives.length > 1) {
+            for (let player of playersWithLives) {
+              await bot.sendMessage(player.chatId, `Congratulations! You guessed the word "${word}"`);
+            }
+            const nextPlayerIndex = (players.indexOf(currentPlayer) + 1) % players.length;
+            const nextPlayer = players[nextPlayerIndex];
+            await startRound(nextPlayer, word, new Set(), Array(word.length).fill(null));
+          } else {
+            for (let player of playersWithLives) {
+              await bot.sendMessage(player.chatId, `Game over! @${player.username} won!! The word was "${word}".`);
+            }
+            gameOver = true;
+          }
+        } else {
+          await bot.sendMessage(currentPlayer.chatId,` @${currentPlayer.username}, good guess! The letter "${letter}" is in the word.`);
+          await bot.sendMessage(currentPlayer.chatId, ` The word now looks like this: ${wordState.join(' ')}`);
         }
       }
 
-      if (!result.includes('_')) {
-        for (let player of players) {
-          bot.sendMessage(player.chatId, `Congratulations! You guessed the word "${word}"`);
+      if (!gameOver) {
+        if (!wordState.includes(null)) {
+          for (let player of players) {
+            await bot.sendMessage(player.chatId, `Congratulations! You guessed the word "${word}"`);
+          }
+          gameOver = true;
+        } else if (players.length === 1) {
+          for (let player of players) {
+            await bot.sendMessage(player.chatId,`Game over! @${player.username} won!! The word was "${word}".`);
+          }
+          gameOver = true;
         }
-        gameOver = true;
-      } else if (players.length === 1) {
-        for (let player of players) {
-          bot.sendMessage(player.chatId, `Game over! The word was "${word}".`);
-        }
-        gameOver = true;
-      }
 
       if (!gameOver) {
         let nextPlayerIndex = (players.indexOf(currentPlayer) + 1) % players.length;
         let nextPlayer = players[nextPlayerIndex];
-        bot.sendMessage(nextPlayer.chatId, `@${nextPlayer.username}, you have ${nextPlayer.lives} lives left. Guess a letter!`);
-        setTimeout(() => {
-          startRound(nextPlayer, word, guessedLetters, players);
-        }, 15000);
-      }
-    }
-  };
+        
+        await startRound(nextPlayer, word, guessedLetters, wordState);
+        bot.off('message', messageHandler);
+      
+      };
+    };
+     
+    };
+  },7500);
 
   bot.on('message', messageHandler);
 }
+
+
+
+bot.onText(/\/multi/, async (msg) => {
+  console.log('multi fired')
+  if(gameStarting){
+    bot.sendMessage(msg.chat.id, 'A game is already in progress. Please wait for it to finish.');
+    return;
+  }
+  gameStarting = true
+  players.push({ chatId: msg.chat.id, username: msg.from.username });
+  await bot.sendMessage(msg.chat.id, `@${msg.from.username} started a game! send /join to join the game.`)
+  gameStarting = true;
+  gameTimer = setTimeout(async () => {
+    if (players.length < 2) {
+      await bot.sendMessage(msg.chat.id, 'Not enough players joined the game. Game cancelled!')
+      gameStarting = false
+    } else {
+      console.log('Game is starting')
+     
+      for (let player of players) {
+        player.lives = 6;
+      }
+      const word = randomWords();
+  console.log(word)
+  let wordState = Array(word.length).fill(null);
+      gameInProgress=true
+      startRound(players[0], word, guessedLetters, wordState);
+    }
+  }, 30000);
+})
+  // if(gameInProgress || gameStarting){
+  //   bot.sendMessage(msg.chat.id, 'A game is already in progress or starting. Please wait')
+  // }else{
+  //   players =[{chatId: msg.chat.id, username:msg.from,username}];
+  //   bot.sendMessage(msg.chat.id, `@${msg.from.username} started a game! send /join to join the game.` )
+  //   gameStarting= true;
+  //   gameTimer =setTimeout(()=>{
+  //     if (players.length < 2){
+  //       bot.sendMessage(msg.chat.id, 'Not enough players joined the game. Game cancelled!')
+  //       gameStarting = false
+  //     }else{
+  //       console.log('Game is starting')
+  //       startGame()
+  //     }
+  //   }, 30000)
+  // }
+
 
 bot.onText(/\/join/, (msg) => {
 if (!gameStarting ) {
@@ -174,8 +261,10 @@ clearTimeout(gameTimer);
 gameStarting = false;
 gameInProgress = false;
 bot.sendMessage(msg.chat.id, 'The game has been ended.');
-}
-});
+bot.removeAllListeners()
+
+}})
+;
 
 bot.on('message', (msg) => {
 if (msg.text && msg.text.toLowerCase() === 'ping') {
